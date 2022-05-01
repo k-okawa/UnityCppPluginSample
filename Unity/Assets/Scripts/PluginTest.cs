a9a49f673a63d17c3dcc150417f3b2c67d4ec881
Kyohei <kurokenken0310@gmail.com>
2022-05-01 19:46:41 +0900

    dll import修正

--- Unity/Assets/Scripts/PluginTest.cs
+++ Unity/Assets/Scripts/PluginTest.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 
 public class PluginTest {
-#if !UNITY_EDITOR && UNITY_IOS 
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
     [DllImport ("__Internal")]
 #else
     [DllImport ("SamplePlugin")]
 #endif
     public static extern int test();
 
-#if !UNITY_EDITOR && UNITY_IOS
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
     [DllImport ("__Internal")]
 #else
     [DllImport ("SamplePlugin")]
