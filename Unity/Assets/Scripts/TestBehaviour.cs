65c19d4580536de6a0ef8b22f2117d848a0be518
Kyohei <kurokenken0310@gmail.com>
2022-05-01 19:38:20 +0900

    log追加

--- Unity/Assets/Scripts/TestBehaviour.cs
+++ Unity/Assets/Scripts/TestBehaviour.cs
@@ -1,11 +1,18 @@
 using TMPro;
 using UnityEngine;
 
-public class TestBehaviour : MonoBehaviour {
+public class TestBehaviour : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _logText;
     [SerializeField] private TextMeshProUGUI _testText;
     [SerializeField] private TextMeshProUGUI _helloWorldText;
     
-    void Start() {
+    void Start()
+    {
+        Application.logMessageReceived += Log;
+        
+        Debug.Log("StartCall");
+        
         int result = PluginTest.test();
         Debug.Log($"PluginResult:{result}");
         _testText.text = $"TestResult:{result}";
@@ -13,5 +20,10 @@ public class TestBehaviour : MonoBehaviour {
         string helloWorld = PluginTest.helloWorld();
         Debug.Log(helloWorld);
         _helloWorldText.text = $"HelloWorld:{helloWorld}";
+    }
+
+    void Log(string condition, string stackTrace, LogType type)
+    {
+        _logText.text += $"{condition}\n";
     }
 }
