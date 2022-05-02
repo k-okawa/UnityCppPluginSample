fa597f97f68c1c025a34d9763403bd2f1ba56426
k-okawa <kyohei.okawa@10antz.co.jp>
2022-05-02 19:26:56 +0900

    fix android project

--- Native/projects/android/Android.mk
+++ Native/projects/android/Android.mk
@@ -3,10 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := SamplePlugin_shared
-LOCAL_MODULE_FILENAME := SamplePlugin
+LOCAL_MODULE_FILENAME := libsampleplugin
 LOCAL_CFLAGS    := -Werror
-CPP_FILES := $(LOCAL_PATH)/../../src/library.cpp
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../../src/library.cpp
+CPP_FILES := $(shell find $(LOCAL_PATH)/../../src -name *.cpp)
+LOCAL_SRC_FILES += $(CPP_FILES:$(LOCAL_PATH)/%=%)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../src
 LOCAL_LDLIBS    := -llog
 
--- Unity/Assets/Scripts/PluginTest.cs
+++ Unity/Assets/Scripts/PluginTest.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 
 public class PluginTest {
-#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+#if !UNITY_EDITOR && UNITY_IOS
     [DllImport ("__Internal")]
 #else
-    [DllImport ("SamplePlugin")]
+    [DllImport ("sampleplugin")]
 #endif
     public static extern int test();
 
-#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+#if !UNITY_EDITOR && UNITY_IOS
     [DllImport ("__Internal")]
 #else
-    [DllImport ("SamplePlugin")]
+    [DllImport ("sampleplugin")]
 #endif
     public static extern string helloWorld();
 }
